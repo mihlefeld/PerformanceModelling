312ea690aee8c1b78a4b434b46eb9243f327b0da
malte <asd>
2021-01-05 15:15:16 +0100

    i dont know hat i'm doing

--- main.cpp
+++ main.cpp
@@ -3,13 +3,8 @@
 #include "matrix.h"
 
 int main(int argc, char** argv) {
-    if(argc != 2) {
+    if (argc != 2) {
         std::cout << "Usage: " << argv[0] << " <filename>" << std::endl;
         return 0;
     }
-
-    auto matrices = load_from_file(argv[1]);
-
-    //hello_world();
-    return 0;
 }
